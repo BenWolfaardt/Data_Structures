1dd65d50dd1791f1ddf463be8cffd29382961d41
BenWolfaardt <benjaminwolfaardt@gmail.com>
2021-12-29 19:26:33 +0100

    comments size check

--- 01-data_structures/01-arrays_and_strings/001-is_unique.py
+++ 01-data_structures/01-arrays_and_strings/001-is_unique.py
@@ -1,6 +1,23 @@
-# Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+#------------------------------------------------------Question------------------------------------------------------#
 
-#44 try a hash table
-#117 bit vector?
-#132 try solve in O(n log n)
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot use 
+# additional data structures?
+
+#--------------------------------------------------------Tips--------------------------------------------------------#
+
+# 044 - try a hash table
+# 117 - bit vector?
+# 132 - try solve in O(n log n)
+
+#-----------------------------------------------Questions_&_Assumptions----------------------------------------------#
+
+# You should first ask your interviewer if the string is an ASCII string or a Unicode string. Asking this question
+# will show an eye for detail and a solid foundation in computer science. We'll assume for simplicity the char­
+# acter set is ASCII. If this assumption is not valid, we would need to increase the storage size.
+
+#------------------------------------------------------Solution------------------------------------------------------#
+
+
+
+#--------------------------------------------------------Tests-------------------------------------------------------#
 
