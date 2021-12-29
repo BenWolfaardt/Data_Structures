516245d5f4d51cd25304eb8d3f3fba934fdefe46
BenWolfaardt <benjaminwolfaardt@gmail.com>
2021-12-29 19:26:05 +0100

    comments size check

---
+++ 01-data_structures/01-arrays_and_strings/001-is_unique.py
@@ -0,0 +1,6 @@
+# Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+
+#44 try a hash table
+#117 bit vector?
+#132 try solve in O(n log n)
+
