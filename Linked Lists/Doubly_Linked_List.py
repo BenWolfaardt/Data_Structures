4d4f702ef46c634a9302b8924434ab5e7b86c31f
Ben <benjaminwolfaardt@gmail.com>
2022-12-18 17:05:39 +0100

    Placeholder added

---
+++ Linked Lists/Doubly_Linked_List.py
@@ -0,0 +1,48 @@
+from Singly_Linked_List import SinglyLinkedList
+from random import randint
+
+# A linked list is created by using the node class. We create a Node object and create another class to use this node object. 
+# We pass the appropriate values through the node object to point the to the next data elements. 
+
+class DoublyLinkedListNode:
+
+    def __init__(self, value, nextNode=None, prevNode=None):
+        pass
+
+    def __str__(self):
+        pass
+class DoublyLinkedList(SinglyLinkedList):
+
+    def __init__(self, values=None):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def add(self, value):
+        pass
+
+    def add_to_beginning(self, value):
+        pass
+
+    def add_multiple(self, values):
+        pass
+
+    def pop_head(self):
+        pass
+
+    def generate(self, n, min_value, max_value):
+        pass
+
+    def removeAt(self, index):
+        pass
+
+    def add(self, value):
+        pass
+        
---
+++ Trees, Tries & Graphs/Graph.py
@@ -0,0 +1,27 @@
+class Graph():
+
+    def __init__(self):
+        pass
+
+    def addNode(self, x):
+        pass
+
+    def getNodes(self):
+        pass
+
+# the below will most likely be used in a trees implementation.
+# you can then call it into here to be used
+
+class Node():
+
+    def __init__(self, vertex, adjacentLength):
+        pass
+
+    def addAdjacent(self, x):
+        pass
+        
+    def getAdjacent(self):
+        pass
+
+    def getVertex(self):
+        pass
